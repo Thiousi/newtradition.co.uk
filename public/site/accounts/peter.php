7417cfb68e424e3a47396ffb96eb4f9a6b7fb073
Peter Tait <contact@petertait.com>
2015-06-25 22:25:19 +0100

    update gitignore + account

---
+++ public/site/accounts/peter.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: peter
+email: contact@petertait.com
+password: >
+  $2a$10$RqeUcV3/ymApQovt7vjKcuL9KznwqsIfqGaWcw0.ufCUocF/ZysYW
+language: en
+role: admin
