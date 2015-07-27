9997807001023051318c565490423e6b1ede9d62
Peter Tait <contact@petertait.com>
2015-07-27 23:18:05 +0100

    fix viewport sizes

--- public/site/controllers/journal.php
+++ public/site/controllers/journal.php
@@ -1,14 +1,11 @@
 <?php return function($site, $pages, $page) {
 
-  // fetch the basic set of pages
   $articles = $site->find('journal')->children()->visible()->sortBy('date', 'desc')->limit(7);
 
-  // add the tag filter
   if($tag = param('tag')) {
     $articles = $articles->filterBy('tags', $tag, ',');
   }
-
-  // fetch all tags
+  
   $tags = $articles->pluck('tags', ',', true);
 
   // apply pagination
