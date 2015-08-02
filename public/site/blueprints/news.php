8f1abc2e93b0231062900633ab005da6ec807d50
Peter Tait <contact@petertait.com>
2015-08-02 22:47:10 +0100

    add news blueprint

---
+++ public/site/blueprints/news.php
@@ -0,0 +1,46 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: News
+pages: false
+files:
+  sortable: true
+fields:
+  title:
+    label: Title
+    type:  text
+    required: true
+  date:
+    label: Date
+    type:  date
+    width: 1/4
+  author:
+    label: Author
+    type: user
+    width: 1/2
+  toggle:
+    label: Feature Post
+    type: checkbox
+    text: Featured
+    width: 1/4
+  line-a:
+    type: line
+  text:
+    label: Excerpt
+    type:  textarea
+  text:
+    label: Article
+    type:  textarea
+    required: true
+    size: large
+  line-b:
+    type: line
+  comments:
+    label: Comments
+    type: checkbox
+    text: show
+  relatedlinks:
+    label: Related Links
+    type:  tags
+  tags:
+    label: Tags
+    type:  tags
