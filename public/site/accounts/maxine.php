89d479e2d21260e4be1a909af1e0b982a49fd05c
Peter Tait <contact@petertait.com>
2015-09-22 23:53:15 +0100

    fix mobile spacings

--- public/site/accounts/maxine.php
+++ public/site/accounts/maxine.php
@@ -9,10 +9,10 @@ password: >
 language: en
 role: admin
 history:
+  - work/axa-wealth
+  - work/axa-wealth-test
   - >
     journal/the-bebanking-talk-i-nearly-gave-at-tedxcheltenham
   - >
     journal/to-create-an-online-financial-advice-brand-become-a-follower-of-fashion
   - work/thames-valley-police
-  - work/paraplanners-powwow
-  - work/the-home-office
